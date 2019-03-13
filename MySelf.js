62d231c4882f200e962b039a167375c62f134122
joyroy-jr <48515566+joyroy-jr@users.noreply.github.com>
2019-03-13 17:04:33 +0600

    Create MySelf.js

---
+++ MySelf.js
@@ -0,0 +1,12 @@
+name= 'Joy Roy'
+birthday= (2019-2002)
+department='computer'
+roll= 967018
+
+
+console.log('my name is '+ name + ', i am ' + birthday +', currently i am studying in daffodil polytechnic instute on computer engeneearing and there my roll is ' +roll)
+
+
+
+
+// 'My name is Joy Roy I am 17 currently i am studying in daffodil polytechnic instute on computer engeneearing and there my roll is 967018'
