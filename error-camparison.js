c5ae5f837812d96488e5f3963d0ca86f3ff4177a
joyroy-jr <48515566+joyroy-jr@users.noreply.github.com>
2019-03-20 16:57:44 +0600

    Create error-camparison.js

---
+++ error-camparison.js
@@ -0,0 +1,15 @@
+century=101
+doblecentury=201
+
+
+
+if(century === 101)
+console.log("i made a century")
+else{
+  console.log("I DONT KNOW HOW TO PLAY CRIKET")
+}
+if(doblecentury===201)
+console.log("i made a doblecentury")
+else{
+console.log("I DONT KNOW HOW TO PLAY CRIKET")
+}
